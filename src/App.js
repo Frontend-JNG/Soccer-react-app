4c22bab8ed248176c7e7600c16d42b325ea5b8dc
Jen Perez <93927905+jenperez613@users.noreply.github.com>
2022-08-12 18:31:18 -0400

    front page using mui for styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Content from "./components/Content";
+import {
+  CssBaseline,
+  ThemeProvider,
+} from "@mui/material";
+import { theme } from "./themes/theme";
 
 function App() {
   return (
-    <div className="App">
+    <ThemeProvider theme={theme}>
+      <CssBaseline enableColorScheme />
+
       <Navbar />
       <Content />
       <Footer />
-    </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/Content.js
+++ src/components/Content.js
@@ -2,22 +2,43 @@ import React, { useState } from "react";
 import "../App.css";
 import Leagues from "./Leagues";
 import Standings from "./Standings";
+import Button from "@mui/material/Button";
+
+import Grid from "@mui/material/Grid";
 
 const Content = () => {
   const [active, setActive] = useState(true);
 
   return (
-    <div className="content-container">
-      <div className="tabs">
-        <div className="tab-leagues" onClick={() => setActive(true)}>
-          <h2 style={{ color: active ? "#c20114" : null }}>Leagues</h2>
-        </div>
-        <div className="tab-standings" onClick={() => setActive(false)}>
-          <h2 style={{ color: !active ? "#c20114" : null }}>Standings</h2>
-        </div>
-      </div>
+    <Grid
+      container
+      spacing={2}
+      display="flex"
+     
+      style={{ justifyContent: "center", marginTop: "5px" }}
+    >
+      <Grid item xs={2}>
+        <Button
+          fullWidth
+          color="primary"
+          variant="contained"
+          onClick={() => setActive(true)}
+        >
+          <h2 style={{ color: active ? "secondary" : null }}>Leagues</h2>
+        </Button>
+      </Grid>
+      <Grid item xs={2}>
+        <Button
+          fullWidth
+          color="primary"
+          variant="contained"
+          onClick={() => setActive(false)}
+        >
+          <h2 style={{ color: !active ? "secondary" : null }}>Standings</h2>
+        </Button>
+      </Grid>
       {active ? <Leagues /> : <Standings />}
-    </div>
+    </Grid>
   );
 };
 
